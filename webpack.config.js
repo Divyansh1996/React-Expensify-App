5448f55846376a79ab21ffc62148d847dc37d2c2
Divyansh1996 <divyanshagrawal96@gmail.com>
2025-08-17 15:53:48 +0530

    New Changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 console.log("dirname = "+__dirname)
 
-module.exports = {
-    entry:"./src/app.js",
-    output:{
-        path: path.join(__dirname,'public'),
-        filename: "bundle.js",
-        publicPath: '/'
-    },
+module.exports = (env) => {
+    const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+    return {
+        entry:"./src/app.js",
+        output:{
+            path: path.join(__dirname,'public'),
+            // filename: "bundle.js",
+            filename: "[name].[contenthash].js",
+            publicPath: '/',
+            clean:true
+        },
+    
 
     // module:{
     //     rules:[{
@@ -30,12 +35,45 @@ module.exports = {
         },
         {
             test: /\.s?css$/,
-            use:['style-loader', 'css-loader', 'sass-loader']
+            // use:['style-loader', 'css-loader', 'sass-loader']
+            use:[MiniCssExtractPlugin.loader, 
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
         }
       ]
     },
+    plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css',
+            }),
+            // optional if you need "process" polyfill
+            new webpack.ProvidePlugin({
+                process: 'process/browser'
+            })
+    ],
+    optimization: {
+        usedExports: true,   // tree-shaking
+        minimize: true,
+        splitChunks: {
+            chunks: "all",     // vendor splitting
+        },
+    },
+    
 
-    devtool:'eval-cheap-module-source-map',
+    devtool:env === 'production'? 'source-map': 'inline-source-map',
     devServer:{
         static: {
             directory: path.join(__dirname, 'public')
@@ -53,9 +91,5 @@ module.exports = {
             "process": require.resolve("process/browser")
         }
     },
-//     plugins: [
-//     new webpack.ProvidePlugin({
-//       process: 'process/browser'
-//     })
-//   ]
-}
+    }
+} 
